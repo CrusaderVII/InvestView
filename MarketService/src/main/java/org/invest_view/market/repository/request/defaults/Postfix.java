8fd15a75e032d1b73ad12138352c2d570a9355f2
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-19 18:51:07 +0300

    I added repository package with defaults prefixes and postfixes to communicate with MOEX API.

---
+++ MarketService/src/main/java/org/invest_view/market/repository/request/defaults/Postfix.java
@@ -0,0 +1,28 @@
+package org.invest_view.market.repository.request.defaults;
+
+public enum Postfix {
+    DEFAULT_GET_FOR_LAST_MONTH_POSTFIX (".json?iss.meta=off&"
+            + ".only=history&sort_column=TRADEDATE&sort_order=desc"),
+
+    DEFAULT_GET_DATES_POSTFIX ("/dates.json"),
+
+    DEFAULT_GET_NOW_POSTFIX (".json?iss.meta=off"),
+
+    DEFAULT_GET_HISTORY_POSTFIX (".json?iss.meta=off&start="),
+
+    DEFAULT_GET_HISTORY_CURSOR_POSTFIX (".json?iss.meta=off&iss.only=history.cursor"),
+
+    DEFAULT_JSON_EXTENSION (".json");
+
+    //DEFAULT_GET_USER_ISSUERS_POSTFIX ("get/user/issuers?id=");
+
+    private final String value;
+
+    private Postfix(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
---
+++ MarketService/src/main/java/org/invest_view/market/repository/request/defaults/Prefix.java
@@ -0,0 +1,22 @@
+package org.invest_view.market.repository.request.defaults;
+
+public enum Prefix {
+    DEFAULT_GET_HISTORY_PREFIX ("http://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/"),
+
+    DEFAULT_GET_NOW_PREFIX ("https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/"),
+
+    DEFAULT_GET_ALL_ISSUERS_PREFIX ("https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities");
+
+    //DEFAULT_GET_USER_ISSUERS_PREFIX("http://localhost:8090/users/");
+
+    private final String value;
+
+    Prefix(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+
+}
