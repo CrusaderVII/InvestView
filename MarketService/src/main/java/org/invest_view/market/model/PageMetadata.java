8abd2eb806b400fc6417c2352b578074126f8a29
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-19 18:40:57 +0300

    I added PageMetadata class.

---
+++ MarketService/src/main/java/org/invest_view/market/model/PageMetadata.java
@@ -0,0 +1,13 @@
+package org.invest_view.market.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class PageMetadata {
+
+    private int pages;
+}
