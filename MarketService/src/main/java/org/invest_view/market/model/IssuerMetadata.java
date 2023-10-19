0917e5cdb853016e4a91d98461dd398879fcf5a3
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-19 18:39:58 +0300

    I added IssuerMetadata class.

---
+++ MarketService/src/main/java/org/invest_view/market/model/IssuerMetadata.java
@@ -0,0 +1,13 @@
+package org.invest_view.market.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class IssuerMetadata {
+    private String secId;
+    private String fullName;
+}
