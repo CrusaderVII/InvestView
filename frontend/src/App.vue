a033815e877d637f43c47f43e29c72de3070eae1
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-24 21:46:09 +0300

    I added main input to main page.

---
+++ frontend/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="input-wrapper">
+      <div class="input-box">
+        <fa class="icon" icon="search"></fa>
+        <input type="search" placeholder="Enter issuer name: GAZP" class="main-input">
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #1A1A1A;
+  }
+
+  .input-wrapper{
+    margin-top: 35vh;
+    width: 40vw;
+    height: 50px;
+    border-radius: 20px;
+    border: 2px solid #42B883;
+    background-color: #2F2F2F;
+  }
+
+  .input-box {
+    display: flex;
+    widows: 100%;
+    height: 100%;
+    align-items: center;
+  }
+
+  .main-input{
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    color: #A0A0A0;
+    font-size: large;
+    padding-left: 10px;
+    background-color: transparent;
+  }
+
+  .icon {
+    color: #A0A0A0;
+    margin-left: 10px;   
+    margin-bottom: 5px; 
+  }
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp({
-    
-}).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas)
+
+createApp(App)
+    .component('fa', FontAwesomeIcon)
+    .mount('#app')
