29ccd12b89aa748957fbe18d29b2b4e0e5b90560
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-28 18:43:57 +0300

    I added design for login form.

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,11 +1,14 @@
 <script setup>
   import { ref } from 'vue';
-    // const mains = ref([new Issuer('GAZP', 'Газпром', 270.1), new Issuer('TATN', 'Татнефть', 619.3), new Issuer('YNDX', 'Яндекс', 2185)])
+
     var showNavbarState = ref(true)
     var changeMainsState = ref(true)
+    var showLoginFormState = ref(false)
+    var showRegisterFormState = ref(false)
 
     const mains1 = ref(null)
     const mains2 = ref(null)
+    const user = ref(null)
 
     fetch('http://localhost:8081/api/market-data/v1/main/mains-now')
       .then(response => response.json())
@@ -29,13 +32,38 @@
       }
     }
 
+    const showRegisterForm = () => {
+      showLoginFormState = false
+      //showRegisterFormState = true
+    }
+
 </script>
 
 <template>
   <main>
+    <div v-if="showRegisterFormState" class="overlay">
+    </div>
+
+    <div v-if="showLoginFormState" class="overlay">
+      <div class="login-form">
+        <div class="close-wrapper">
+          <fa class="close-btn-icon" icon="fa-times" @click="showLoginFormState = false"></fa>
+        </div>
+        <div class="login-input-wrapper">
+          <h1>Login to InvestView</h1>
+          <input type="text" class="login-input" placeholder="email">
+          <input type="password" class="login-input" placeholder="password">
+          <div class="register-link-wrapper">
+            <a class="register-link" @click="showRegisterForm()">Don't have an account? Register!</a>
+          </div>
+        </div>
+        <button class="login-btn">Login</button>
+      </div>
+    </div>
+
     <div class="navbar" id="navbar">
       <ul class="navbar-menu">
-        <li><a>Login</a></li>
+        <li><a @click="showLoginFormState = true">Login</a></li>
         <li><a>About project</a></li>
         <li><a>Contact</a></li>
       </ul>
@@ -84,6 +112,122 @@
     background-color: #1A1A1A;
   }
 
+  .overlay {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    z-index: 10;
+    background-color: rgba(0, 0, 0, 0.77);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .login-form {
+    width: 750px;
+    height: 450px;
+    background-color: #1A1A1A;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-left: 35px;
+    padding-right: 35px;
+  }
+
+  .close-wrapper {
+    width: 100%;
+    height: 75px;
+    display: flex;
+    justify-content: flex-end;
+    background-color: transparent;
+    align-items: center;
+  }
+
+  .close-btn-icon {
+    font-size: xx-large;
+    color: #A0A0A0;
+    transition: 0.3s;
+    cursor: pointer;
+  }
+
+  .close-btn-icon:hover {
+    color:#ff4949;
+  }
+
+  .login-input-wrapper {
+    width: 100%;
+    height: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .login-input-wrapper h1 {
+    color: #A0A0A0;
+  }
+
+  .login-input-wrapper input {
+    background-color: transparent;
+    padding-left: 30px;
+    width: 60%;
+    height: 30px;
+    transition: 0.3s;
+    outline: none;
+    border: 2px solid #A0A0A0;
+    border-radius: 5px;
+    margin-top: 25px;
+    color: #A0A0A0;
+  }
+
+  ::-ms-reveal {
+    filter: invert(100%);
+  }
+
+  .register-link-wrapper {
+    width: 65%;
+    height: 20px;
+    align-items: center;
+    border-bottom: 1px solid #42B883;
+    display: flex;
+    justify-content: center;
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+
+  .register-link-wrapper a {
+    color: #A0A0A0;
+    cursor: pointer;
+  }
+
+  .register-link-wrapper a:hover {
+    color: #42B883;
+  }
+  
+  .login-btn {
+    margin-top: 15px;
+    width: 100px;
+    height: 40px;
+    transition: 0.4s;
+    color: #42B883;
+    border: 2px solid #42B883;
+    border-radius: 5px;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  .login-btn:hover {
+    background-color: #42B883;
+    color: #1A1A1A;
+  }
+
+  .login-input-wrapper input:focus {
+    border-color: #42B883;
+  }
+
+  .login-input-wrapper input:hover {
+    border-color: #42B883;
+  }
   .navbar {
     top: -55px;
     position: fixed;
@@ -112,6 +256,12 @@
   .navbar-menu li {
     list-style: none;
     color: #A0A0A0;
+    transition: 0.25s;
+    cursor: pointer;
+  }
+
+  .navbar-menu li:hover {
+    color: white;
   }
 
   .navbar-switch {
@@ -138,7 +288,7 @@
     width: 40vw;
     height: 50px;
     border-radius: 20px;
-    border: 2px solid #42B883;
+    border: 2px solid #42b883;
     background-color: #2F2F2F;
     min-width: 600px;
   }
